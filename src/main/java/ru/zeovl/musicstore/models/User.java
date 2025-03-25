52657c556831d97a68ef40e2283c66adc93e130a
Sergey Nesterenko <47058928+zeov1@users.noreply.github.com>
2025-03-25 14:59:43 +0300

    user model added

---
+++ src/main/java/ru/zeovl/musicstore/models/User.java
@@ -0,0 +1,64 @@
+package ru.zeovl.musicstore.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotNull
+    @Size(min = 2, max = 100)
+    @Column(name = "username", unique = true)
+    private String username;
+
+    @NotNull
+    @Size(min = 8, max = 150)
+    @Column(name = "password")
+    private String password;
+
+    @NotNull
+    @Email
+    @Column(name = "email")
+    private String email;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+}
+
