e8a8867336d9d99c29d04696783422227847e706
Sergey Nesterenko <47058928+zeov1@users.noreply.github.com>
2025-03-30 10:42:44 +0300

    users TODO

--- src/main/java/ru/zeovl/musicstore/config/SecurityConfiguration.java
+++ src/main/java/ru/zeovl/musicstore/config/SecurityConfiguration.java
@@ -2,19 +2,48 @@ package ru.zeovl.musicstore.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfiguration {
 
     /**
      * Этот метод отключает необходимость авторизации пользоателя для
      * доступа ко всем URL. В будущем поведение будет изменено.
      */
+//    @Bean
+//    public WebSecurityCustomizer webSecurityCustomizer() {
+//        return (web) -> web.ignoring()
+//                .requestMatchers(new AntPathRequestMatcher("/**"));
+//    }
     @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return (web) -> web.ignoring()
-                .requestMatchers(new AntPathRequestMatcher("/**"));
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authorize) -> authorize
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults())
+                .formLogin(Customizer.withDefaults());
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails userDetails = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(userDetails);
     }
 }
---
+++ src/main/sql/add_entries.sql
@@ -0,0 +1,14 @@
+-- users
+
+SELECT *
+FROM users;
+
+INSERT INTO users(username, password, email)
+VALUES ('zeovl', '12345678', 'zeovl@vk.com');
+
+INSERT INTO users(username, password, email)
+VALUES ('pavel', '12345678', 'pavel@example.com');
+
+INSERT INTO users(username, password, email)
+VALUES ('alex', '12345678', 'alex@example.com');
+
